27ee66d9da53f78f432072495316dbc1a733d909
S4NGAL <41579632+S4NGAL@users.noreply.github.com>
2023-12-26 23:18:35 +0200

    :sparkles: Add Teleporter

---
+++ Assets/PlayerTeleporter.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTeleporter : MonoBehaviour
+{
+    private GameObject currentTeleporter;
+    private int randomIterator;
+    private Vector2[] places = {new Vector2(30,-14), new Vector2(60, 12), new Vector2(15, 25)};
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E)) 
+        {
+            if(currentTeleporter != null)
+            {
+                randomIterator = Random.Range(0, 4);
+                transform.position = places[randomIterator];
+            }      
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Teleporter"))
+        {
+            currentTeleporter = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Teleporter"))
+        {
+            if(collision.gameObject == currentTeleporter)
+            {
+                currentTeleporter = null;
+            }
+        }
+    }
+
+}
